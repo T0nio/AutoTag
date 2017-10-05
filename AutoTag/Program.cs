e0e4cc7f8022c6a7551ceaddb7a95aff23d87a96
Antoine <antoine@nebulae.co>
2017-10-05 14:45:01 +0200

    Initial commit

---
+++ AutoTag/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoTag
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
