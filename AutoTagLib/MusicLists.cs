c8c67603e639f75220eed5c6377b6f9fbda93520
Thibaut Seys <thibaut.seys@hotmail.fr>
2017-10-28 16:12:26 +0200

    Adapt model to be used by GUI

--- AutoTagLib/MusicLists.cs
+++ AutoTagLib/MusicLists.cs
@@ -1,27 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace AutoTagLib
 {
-    class MusicLists
+    public class MusicLists
     {
-#region Properties
+
+        #region Properties
+
         public Dictionary<string, List<Musics>> Dict  { get; set; }
-
         public List<string> Extensions { get; set; }
-#endregion
 
-#region Constructor
+        #endregion
+
+        #region Constructor
+
         public MusicLists()
         {
 
         }
-#endregion
 
-#region Methods
+        #endregion
+
+        #region Methods
+
         public List<Musics> CreateList(string path) //Fill an empty list with all the files inside the path (no subfolders) with extensions authorized
         {
             return null;
@@ -52,7 +53,6 @@ namespace AutoTagLib
                     this.Dict.Add(dir, MusicLists.ListFromFileToMusic(dir));
                 }
             }
-
         }
 
         public void FillDict() //Fill a dictionnary with all the subfolders path coming from the path and link it to the list from CreateList
@@ -79,7 +79,7 @@ namespace AutoTagLib
         {
 
         }
-#endregion
 
+        #endregion
     }
 }
