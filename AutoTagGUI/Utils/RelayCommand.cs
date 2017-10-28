5dbd355997b394243a65dbc07f9edbc0421e237b
Thibaut Seys <thibaut.seys@hotmail.fr>
2017-10-28 16:11:55 +0200

    Connect the GUI view to the model

--- AutoTagGUI/Utils/RelayCommand.cs
+++ AutoTagGUI/Utils/RelayCommand.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace System.Windows.Input
+﻿namespace System.Windows.Input
 {
     public class RelayCommand<T> : ICommand
     {
